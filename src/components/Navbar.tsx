447fb121f9fd2091db4530c02afda2668fe9614a
Anthony Sokolovsky <tony9sokol@gmail.com>
2025-11-23 15:38:41 +0200

    EmployeeCard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,25 +2,25 @@ import './Navbar.css';
 import '../../employee.png'
 
 interface NavLink {
-  name: string;
-  path: string;
+    name: string;
+    path: string;
 }
 
 const links: NavLink[] = [
-  { name: 'Employees', path: '/employees' }
+    { name: 'Employees', path: '/employees' }
 ];
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-    <img src="employee.png" alt="Description" width="48" height="48" />
-      <ul className="nav-links">
-        {links.map(link => (
-          <li key={link.name}>
-            <a href={link.path}>{link.name}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
+    return (
+        <nav className="navbar">
+            <img src="employee.png" alt="Description" width="48" height="48" />
+            <ul className="nav-links">
+                {links.map(link => (
+                    <li key={link.name}>
+                        <a href={link.path}>{link.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
 }
