0106e2c15dd60d016c8f553599324ef2cd102af2
tony9sokol <102696084+tony9sokol@users.noreply.github.com>
2025-11-23 11:54:33 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Northwind</h1>
+
+
+      <div className="card">
+      </div>
+    </div>
+  );
+}
+
+export default App;
