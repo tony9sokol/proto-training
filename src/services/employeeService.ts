bc2ca3ade36a0760e8272c3615546e972b5db61b
Anthony Sokolovsky <tony9sokol@gmail.com>
2025-11-26 14:33:58 +0200

    fix comments on reorganization

--- src/services/employeeService.ts
+++ src/services/employeeService.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import type { Employee } from "../modules/Employee";
 import { EMPLOYEES_API_URL } from "../config/api";
 
+export const getEmployees = async (): Promise<Employee[]> => {
+  const res = await axios.get(EMPLOYEES_API_URL);
+  return res.data;
+};
+
 export const employeeService = {
-  async getEmployees(): Promise<Employee[]> {
-    const res = await axios.get(EMPLOYEES_API_URL);
-    return res.data;
-  },
+  getEmployees,
 };
